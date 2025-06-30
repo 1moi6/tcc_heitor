bdb4f1ec0774018acadf37e8f1acb6287e168b2e
HDharder <heitordmp14@gmail.com>
2025-06-30 10:06:18 -0400

    Correção do modo expandido

--- Conjectura de Pontes Expandido.js
+++ Conjectura de Pontes Expandido.js
@@ -117,8 +117,8 @@ function checkF(f) {
 }
 
 // Lista de primos a testar (exemplo)
-const primos = [431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 487, 491, 499, 503, 509, 521, 523, 541, 547, 557, 563, 569, 571, 577, 587, 593, 599, 601, 607, 613, 617, 619, 631, 641, 643, 647, 653, 659, 661, 673, 677, 683, 691, 701, 709, 719, 727, 733, 739, 743, 751, 757, 761, 769, 773, 787, 797, 809, 811, 821, 823, 827, 829, 839, 853, 857, 859, 863, 877, 881, 883, 887, 907, 911, 919, 929, 937, 941, 947, 953, 967, 971, 977, 983, 991];
-let quebrapad = true;
+const primos = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 487, 491, 499, 503, 509, 521, 523, 541, 547, 557, 563, 569, 571, 577, 587, 593, 599, 601, 607, 613, 617, 619, 631, 641, 643, 647, 653, 659, 661, 673, 677, 683, 691, 701, 709, 719, 727, 733, 739, 743, 751, 757, 761, 769, 773, 787, 797, 809, 811, 821, 823, 827, 829, 839, 853, 857, 859, 863, 877, 881, 883, 887, 907, 911, 919, 929, 937, 941, 947, 953, 967, 971, 977, 983, 991];
+let quebrapad = false;
 const quebraVal = [];
 const ValFind = [];
 
@@ -135,14 +135,14 @@ primos.forEach(p => {
 
     if (r1 || r2) {
       quebralocal = false;
-      quebrapad = false;
-      if (r1) ValFind.push(`${p + V}(${p} + ${V})`);
-      if (r2) ValFind.push(`${p - V}(${p} - ${V})`);
+      if (r1) ValFind.push(`${p + V}(${p} + ${V})`); else ValFind.push(`${p - V}(${p} - ${V})`)
+      //if (r2) ValFind.push(`${p - V}(${p} - ${V})`);
       break;
     }
   }
   if (quebralocal){
     quebraVal.push(p);
+    quebrapad = true;
     //console.log(`\n❗ Quebra de padrão detectada em p = ${p}. Nenhum primo encontrado dentro do offset.`);
   }
 });
@@ -152,4 +152,5 @@ if (!quebrapad) {
 } else {
   console.log(`\n❗ Quebra de padrão detectada em p = ${quebraVal}. Nenhum primo encontrado.`);
 }
-console.log(`Valores de f: ${ValFind}`)
+console.log(`Quantidade de números analisados: `, primos.length)
+console.log(`Valores de f: `, ValFind.map(String));
